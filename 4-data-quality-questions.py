04ee05efac44dd818400c06e7a87efb3822a6cdd
Tapasswin <manchala.tapasswin955@gmail.com>
2026-05-21 23:27:00 +0530

    data validation

---
+++ 4-data-quality-questions.py
@@ -0,0 +1,18 @@
+import polars as pl
+from cuallee import Check, CheckLevel
+
+# Read CSV file into Polars DataFrame
+df = pl.read_csv("./data/sample_data.csv")
+
+# Question: Check for Nulls on column Id and that Customer_ID column is unique
+# check docs at https://canimus.github.io/cuallee/polars/ on how to define a check and run it.
+# you will end up with a dataframe of results, check that the `status` column does not have any "FAIL" in it
+
+check = Check(CheckLevel.ERROR, "Completeness")
+
+validate = (check.is_complete("Customer_ID").is_unique("Customer_ID").validate(df))
+
+print(validate)
+
+result = validate["status"].to_list()
+assert "FAIL" not in result
